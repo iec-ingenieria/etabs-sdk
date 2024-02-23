863f83daee0a473c186c39fd869acd53c4f0c2df
mgv <mglasner@iec.cl>
2024-02-23 13:08:40 -0300

    métodos para rescatar datos de tablas etabs

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""Main."""
+from pathlib import Path
+
 from etabs_sdk import Etabs
 
-etabs = Etabs(modelo_nuevo=True)
+# path = Path("C:\\Users\\User\\Desktop\\Modelo en trabajo\\test.EDB")
+# path2 = Path("C:\\Users\\User\\Desktop\\Modelo en trabajo\\test123.EDB")
+
+etabs = Etabs(adjuntar_a_instancia=True)
+columnas_tabla = etabs.columnas_tabla("Area Assignments - Pier Labels", printable=True)
+datos_tabla = etabs.datos_tabla("Area Assignments - Pier Labels")
+print(datos_tabla)
+# etabs.abrir_modelo(path)
+# etabs.cerrar_modelo()
+# etabs = Etabs(modelo_nuevo=True)
